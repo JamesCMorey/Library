6156cea9069ec37277e4e00997d7c620f980a094
JamesCMorey <jmorey@tutanota.com>
2024-02-04 15:06:16 -0600

    Made add_entry use memcpy and memcmp
    Trying to move towards type agnostic functions and away from ones that operate
    solely on strings.

--- hashmap/hashmap.c
+++ hashmap/hashmap.c
@@ -6,6 +6,7 @@
 #define SIZE 4096
 
 struct hm_entry {
+	int defined;
 	void *key;
 	void *value;
 };
@@ -17,7 +18,7 @@ struct hashmap {
 };
 
 unsigned long hash(unsigned char *str);
-void init_hm(struct hashmap **hm, size_t capacity, int typesize, int arraysize);
+void init_hm(struct hashmap **hm, size_t capacity, int arraysize, int typesize);
 void add_entry(struct hashmap *hm, void *key, void *value);
 struct hm_entry *get_entry(struct hashmap *hm, void *key);
 void print_hm(struct hashmap *hm);
@@ -26,29 +27,20 @@ void del_entry(struct hashmap *hm, void *key);
 int main()
 {
 	struct hashmap *test;
-	char key[SIZE] = "9ijes";
-	char value[SIZE] = "2ji3o";
+	char key[SIZE] = "iojweif";
+	char value[SIZE] = "ajfoewijf";
 
-	// hm mani
-	init_hm(&test, 12, sizeof(char), 4096);
+	init_hm(&test, 12, SIZE, sizeof(char));
+	add_entry(test, key, value);
 	add_entry(test, "test", "sendit");
+	add_entry(test, "test", "sendit");
+	add_entry(test, "asdf", "sendit");
 	add_entry(test, "asdf", "sendit");
 	add_entry(test, "iwoef", "sendit");
 	add_entry(test, "asdfasfe", "sendit");
-	add_entry(test, key, value);
 
 	print_hm(test);
 	return 0;
-}
-
-void print_hm(struct hashmap *hm)
-{
-	struct hm_entry entry;
-	for (int i = 0; i < hm->capacity; i++) {
-		entry = hm->entries[i];
-		printf("Index: %d; Key: %s; Value: %s\n", i, entry.key,
-								entry.value);
-	}
 }
 
 struct hm_entry *get_entry(struct hashmap *hm, void *key)
@@ -65,7 +57,7 @@ struct hm_entry *get_entry(struct hashmap *hm, void *key)
 void add_entry(struct hashmap *hm, void *key, void *value)
 {
 	int index = hash(key) % hm->capacity;
-	printf("Index: %d\n", index);
+	printf("Index: %d; Key: %s\n", index, key);
 
 	if (hm->size == hm->capacity) {
 		puts("Hashmable is full");
@@ -75,29 +67,38 @@ void add_entry(struct hashmap *hm, void *key, void *value)
 	// look for index not already in use and check to make sure key is not
 	// already in use
 	// convert void * to char * so that it can be indexed with [] operator
-	struct hm_entry entry = hm->entries[index];
-	while (((char *)entry.key)[0] != '\0') {
+	struct hm_entry *entry = &hm->entries[index];
+	// need to add wrap around if goes past last index
+	while (entry->defined) {
 
-		if (!strcmp(entry.key, key)) {
-			printf("Key (%s, %s) already in use\n", key, entry.key);
+		printf("Entry->key: %s\n", entry->key);
+		if (!memcmp(entry->key, key, SIZE -1)) {
+			printf("Key (%s, %s) already in use\n", key, entry->key);
 			return;
 		}
 
-		entry = hm->entries[index++];
+		if(++index >= hm->capacity) {
+			index = 0;
+		}
+
+		entry = &hm->entries[index];
 	}
 
 	// copy the data in so that they all point to different memory locations
-	strncpy(entry.key, key, SIZE - 1);
-	strncpy(entry.value, value, SIZE - 1);
+	entry->defined = 1;
+	memcpy(entry->key, key, SIZE);
+	memcpy(entry->value, value, SIZE);
+
+	hm->size++;
 }
 
-void init_hm(struct hashmap **hm, size_t capacity, int typesize, int arraysize)
+void init_hm(struct hashmap **hm, size_t capacity, int arraysize, int typesize)
 {
 	*hm = malloc(sizeof(struct hashmap));
 
 	// need everything to be set to 0 for adding entries
 	(**hm).entries = calloc(capacity, sizeof(struct hm_entry));
-	// give char * vars in entry some memory to memcpy to later
+	// give void * vars in entry some memory to memcpy to later
 	for (int i = 0; i < capacity; i++) {
 		(**hm).entries[i].key = calloc(arraysize, typesize);
 		(**hm).entries[i].value = calloc(arraysize, typesize);
@@ -105,6 +106,16 @@ void init_hm(struct hashmap **hm, size_t capacity, int typesize, int arraysize)
 
 	(**hm).capacity = capacity;
 	(**hm).size = 0;
+}
+
+void print_hm(struct hashmap *hm)
+{
+	struct hm_entry entry;
+	for (int i = 0; i < hm->capacity; i++) {
+		entry = hm->entries[i];
+		printf("Index: %d; Defined: %d; Key: %s; Value: %s\n",
+				i, entry.defined, entry.key, entry.value);
+	}
 }
 
 // djb2: http://www.cse.yorku.ca/~oz/hash.html
